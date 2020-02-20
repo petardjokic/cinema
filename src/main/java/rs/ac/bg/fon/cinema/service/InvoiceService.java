387ec7782ff8786a10369dedfec4bfbc92648e8a
Petar Djokic <petar.djokic@mozzartbet.com>
2020-02-20 18:12:27 +0100

    s

--- src/main/java/rs/ac/bg/fon/cinema/service/InvoiceService.java
+++ src/main/java/rs/ac/bg/fon/cinema/service/InvoiceService.java
@@ -1,8 +1,11 @@
 package rs.ac.bg.fon.cinema.service;
 
+import rs.ac.bg.fon.cinema.domain.Invoice;
 import rs.ac.bg.fon.cinema.service.dto.InvoiceDto;
 
 public interface InvoiceService {
 	
 	public InvoiceDto getInvoiceById(Long invoiceId);
+	
+	public Invoice saveInvoice();
 }
